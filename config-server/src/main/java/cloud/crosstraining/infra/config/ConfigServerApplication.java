f6f435324ec6344d4c454dfddc7d39b5ab83f27a
FlavioLionelRita <flaviolrital@hotmail.com>
2024-06-15 14:30:10 +0200

    refactor: use main gradle
    #2

---
+++ config-server/src/main/java/cloud/crosstraining/infra/config/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+/**
+ * This package contains configuration classes for the cloud cross-training infrastructure.
+ */
+package cloud.crosstraining.infra.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
